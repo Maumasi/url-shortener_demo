05ba60b2ada3c6d6e93f9415e9ba34964a6f071a
maumasi <liu.maumasi@gmail.com>
2016-09-29 04:13:31 -0400

    js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(function(){
 	
 	$.ajax({
 		type: 'GET',
-		url: 'https://http://localhost:3000/api/v1/status',
+		url: 'https://localhost:3000/api/v1/status',
 		success: function(data) {
 			console.log('success ', data);
 		}
